c75d8d521e972cf8706bf950e38fd94124f3f1ae
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2023-01-30 00:25:57 +0530

    config vercel

--- server/src/controllers/blogController.js
+++ server/src/controllers/blogController.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require('mongoose');
 
 
 const addBlog = async (req, res) => {
-    console.log('hello');
+    res.send('hello')
     const { title, description, author } = req.body;
     try {
         if (!title || !description || !author) {
