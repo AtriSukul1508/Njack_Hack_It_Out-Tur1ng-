52b83335151b559b9195b9c390582ab461e7e71d
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2023-02-05 12:33:39 +0530

    token expiration update

--- tur1ng/src/Components/Home/Home.jsx
+++ tur1ng/src/Components/Home/Home.jsx
@@ -6,23 +6,24 @@ import { useAuthContext } from '../../hooks/useAuthContext';
 import { useLogout } from '../../hooks/useLogout';
 import { ToastContainer, toast } from 'react-toastify';
 const Home = () => {
-  // const { user } = useAuthContext();
-  // const { logout } = useLogout();
-  // const isTokenExpired = (token) => {
-  //   const payloadBase64 = token.split('.')[1];
-  //   const decodedJson = Buffer.from(payloadBase64, 'base64').toString();
-  //   const decoded = JSON.parse(decodedJson)
-  //   const exp = decoded.exp;
-  //   const expired = (Date.now() >= exp * 1000)
-  //   return expired
-  // }
-  // if (isTokenExpired(user.token)) {
-  //   logout();
-  //   toast.error("Token expired", {
-  //     position: toast.POSITION.BOTTOM_RIGHT
+  const { user } = useAuthContext();
+  const { logout } = useLogout();
+  const isTokenExpired = (token) => {
+    const payloadBase64 = token.split('.')[1];
+    const decodedJson = atob(payloadBase64)
+    const decoded = JSON.parse(decodedJson)
+    const exp = decoded.exp;
+    const expired = (Date.now() >= exp * 1000)
+    return expired
+  }
+  if (isTokenExpired(user.token)) {
+    // console.log('token expired')
+    // logout();
+    toast.error("Token expired", {
+      position: toast.POSITION.BOTTOM_RIGHT
 
-  //   });
-  // }
+    });
+  }
   return (
     <>
       <div className='home__container'>
