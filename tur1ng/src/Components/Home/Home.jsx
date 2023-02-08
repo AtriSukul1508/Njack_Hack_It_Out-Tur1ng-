eb81bd71ab8b4a36c9346f400fbc70ad6c4c851e
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2023-02-09 02:36:16 +0530

    token expired popover

--- tur1ng/src/Components/Home/Home.jsx
+++ tur1ng/src/Components/Home/Home.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeLeftComponent from './SuggestiveBlogs/HomeLeftComponent';
 import HomeRightComponent from './FeedBlogs/HomeRightComponent';
 import '../../styles/home.css';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useLogout } from '../../hooks/useLogout';
 import { ToastContainer, toast } from 'react-toastify';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 const Home = () => {
   const { user } = useAuthContext();
   const { logout } = useLogout();
+  const [open, setOpen] = useState(false);
   const isTokenExpired = (token) => {
     const payloadBase64 = token.split('.')[1];
     const decodedJson = atob(payloadBase64)
@@ -16,9 +24,13 @@ const Home = () => {
     const expired = (Date.now() >= exp * 1000)
     return expired
   }
+
+  const handlePermission = () => {
+    setOpen(false);
+    logout();
+  }
   if (isTokenExpired(user.token)) {
-    // console.log('token expired')
-    // logout();
+    setOpen(true);
     toast.error("Token expired", {
       position: toast.POSITION.BOTTOM_RIGHT
 
@@ -30,7 +42,21 @@ const Home = () => {
         <HomeLeftComponent />
         <HomeRightComponent />
         <ToastContainer />
-
+        <Dialog
+          open={open}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogContent>
+            <DialogTitle style={{color:'#6d7993',fontWeight:'bold'}}>{"Whoops, Your session has expired"}</DialogTitle>
+            <DialogContentText id="alert-dialog-description" style={{textAlign:'center', color: '#2d2c39', fontFamily: 'Poppins' }}>
+              No Worry,simply login again
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions  style={{display:'flex',justifyContent:'center',marginBottom:'.8rem'}}>
+            <Button onClick={handlePermission} style={{ color: '#fff',background:'#6d7993',borderRadius:'8px',padding:'4px 8px' }}>Login</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     </>
   )
--- tur1ng/src/api.config.js
+++ tur1ng/src/api.config.js
@@ -1,4 +1,4 @@
 export default {
-    'URL': 'https://tur1ng-api.onrender.com',
-    // URL: 'http://localhost:8000',
+    // 'URL': 'https://tur1ng-api.onrender.com',
+    URL: 'http://localhost:8000',
 }
