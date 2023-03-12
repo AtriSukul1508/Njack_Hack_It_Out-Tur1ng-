85e975f4d4c15980e812e1121b66c15c9ef7674f
TuhinBanerjee31 <tuhinbanerjee0231@gmail.com>
2023-03-12 15:57:19 +0530

    Enhanced Error UI

--- tur1ng/src/Components/Error/Error.jsx
+++ tur1ng/src/Components/Error/Error.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "../../styles/error.css";
+import error from "../../Assets/turing-error.png";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
-  return (
-    <>
-      <div className='error-page' style={{width:'100%',height:"100vh",display:"flex",justifyContent:'center',alignItems:'center'}}>
-        <div className='error-div' style={{display:"flex",flexDirection:"column",alignItems:'center',gap:'.9rerm'}}>
-          <h1 style={{fontSize:"3.5rem",fontWeight:"bold",fontStyle:"italic",color:"#5f75a6"}}>404 !!</h1>
-          <p style={{fontFamily:"cursive"}}>The page you're looking for does not exist !!In case you feel lost, click the button below</p>
-          <NavLink className='home-page-btn' to='/' style={{color:'white',backgroundColor:"#2d2c39",padding:"10px",borderRadius:"8px",border:"none",outline:"none",textDecoration:"none"}}>Get Back to Home</NavLink>
-        </div>
-      </div>
-    </>
-  )
-}
+    return(
+        <div className="erro">
+            <div className="erro-content">
+                <h5>Ooopsss Something Went Wrong !!!</h5>
+                <h1>Error 404</h1>
+                <h5>Hey Buddy, looks like you've a page that doesn't exist.</h5>
+                <NavLink to="/"><button>Go Button</button></NavLink>
+            </div>
 
-export default Error
+            <div className="logo-set">
+            <img className="erro-logo" src={error} alt="error-logo" />
+            <div class="home__shadow"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
