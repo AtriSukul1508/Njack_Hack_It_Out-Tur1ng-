65b0f21075db4597de85bdd367fb7498bcc2c8ec
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2023-03-30 23:59:01 +0530

    small change

--- tur1ng/src/api.config.js
+++ tur1ng/src/api.config.js
@@ -1,4 +1,4 @@
 export default {
     // 'URL': 'https://tur1ng-api.onrender.com',
-    'URL': 'https://njackhackitout-tur1ng-production.up.railway.app/',
+    'URL': 'https://njackhackitout-tur1ng-production.up.railway.app',
 }
--- tur1ng/src/hooks/useLogin.js
+++ tur1ng/src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     const login = async (email, password) => {
         setIsLoading(true);
         setError(null);
-        const resp = await fetch('/userapi/login', {
+        const resp = await fetch(apiConfig.URL + '/userapi/login', {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json',
