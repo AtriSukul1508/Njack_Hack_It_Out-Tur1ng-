15259b819461b923dce08a6429ef4ef4544dd98f
AtriSukul1508 <95478617+AtriSukul1508@users.noreply.github.com>
2023-04-01 12:08:54 +0530

    minor change

--- server/src/routes/blogRouter.js
+++ server/src/routes/blogRouter.js
@@ -2,11 +2,11 @@ const blogRouter = require('express').Router();
 const { addBlog, getAllBlogs, getUserSpecificBlogs, deleteBlog, getBlogById, getUpvoteCount, updateUpvoteCount, getSuggestiveBlogs, updateBlogById, getTotalViews } = require('../controllers/blogController');
 const verifyAuth = require('../middlewares/authorization');
 
+blogRouter.use(verifyAuth);
 blogRouter.get('/allblogs', getAllBlogs);
 blogRouter.post('/addblog', addBlog);
 blogRouter.get('/userallblog', getUserSpecificBlogs);
 blogRouter.get('/getsuggestiveblog', getSuggestiveBlogs);
-// blogRouter.use(verifyAuth);
 blogRouter.get('/blog/:id', getBlogById);
 blogRouter.delete('/deleteblog/:id', deleteBlog);
 blogRouter.get('/upvote/:id', getUpvoteCount);
