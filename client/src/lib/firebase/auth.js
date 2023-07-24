1357422418d15d3b4cc4bd0399f7b2761e1cdca1
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-07-24 12:11:21 +0530

    update

--- client/src/lib/firebase/auth.js
+++ client/src/lib/firebase/auth.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA16OPiu-AU9kymLmeyfIbScEt6UIXVd3c",
-  authDomain: "test-419c2.firebaseapp.com",
-  projectId: "test-419c2",
-  storageBucket: "test-419c2.appspot.com",
-  messagingSenderId: "903258556629",
-  appId: "1:903258556629:web:91c83e13de877b26247a5c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
